b4f8b9989bd48d84724354d80494978dcdb0d079
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 21:19:06 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    #render 'birds/index.html.erb'
+    render plain: "Hello #{@birds[3].name}"
   end
 end
