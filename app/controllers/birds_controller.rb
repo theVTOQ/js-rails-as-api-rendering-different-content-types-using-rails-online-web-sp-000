7be9e1db82332b56d2f00cab48fd1687c6165d89
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 21:47:59 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,6 +8,6 @@ class BirdsController < ApplicationController
     #render json: { message: 'Hashes of data will get converted to JSON' }
     #render json: ['As','well','as','arrays']
     #render json: @birds
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] }
   end
 end
