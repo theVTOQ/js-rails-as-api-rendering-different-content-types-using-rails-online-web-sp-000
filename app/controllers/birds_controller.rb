f9a3d2a2374418a1b7892b8bceef8407a2c8a761
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 21:47:55 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
+    #@birds = Bird.all
+    birds = Bird.all
     #render 'birds/index.html.erb'
     #render plain: "Hello #{@birds[3].name}"
     #render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
